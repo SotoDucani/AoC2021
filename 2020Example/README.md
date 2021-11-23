54caa02ca7c6ee3ed7bec1d49c6fa5df95a8ddc3
Joshua Herrmann <jdvh1292@gmail.com>
2021-11-22 23:00:05 -0500

    2020 example solve

---
+++ 2020Example/README.md
@@ -0,0 +1,3 @@
+# Example
+
+Quick example solve of part 1 and 2 from 2020's Day 1 challenge. Making sure I have everything setup well.
---
+++ 2020Example/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	read "github.com/SotoDucani/AoC2021/internal/read"
+)
+
+func part1() {
+	array := read.ReadIntArray("./input.txt")
+	for x := 0; x < len(array); x++ {
+		value1 := array[x]
+		for y := (x + 1); y < len(array); y++ {
+			value2 := array[y]
+			if (value1 + value2) == 2020 {
+				fmt.Printf("Part 1: %d\n", (value1 * value2))
+			}
+		}
+	}
+}
+
+func part2() {
+	array := read.ReadIntArray("./input.txt")
+	for x := 0; x < len(array); x++ {
+		value1 := array[x]
+		for y := (x + 1); y < len(array); y++ {
+			value2 := array[y]
+			for z := (x + 2); z < len(array); z++ {
+				value3 := array[z]
+				if (value1 + value2 + value3) == 2020 {
+					fmt.Printf("Part 2: %d\n", (value1 * value2 * value3))
+				}
+			}
+		}
+	}
+}
+
+func main() {
+	part1()
+	part2()
+}
